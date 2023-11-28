8c7d9bb21e1929ae829569b602c9799a00266618
marco <marcopiovesan2005@gmail.com>
2023-11-28 11:21:45 +0100

    Modifica Stadio togliendo gli errori

--- src/Stadio.java
+++ src/Stadio.java
@@ -5,23 +5,13 @@ import java.awt.event.MouseEvent;
 import java.util.List;
 
 public class Stadio extends Thread{
-    private Tribuna a;
-    private Tribuna b;
-    private Tribuna c;
-    private Tribuna d;
-    public Stadio(Tribuna a, Tribuna b, Tribuna c, Tribuna d) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
+    public Stadio() {
+
     }
 
     @Override
     public void run() {
-        a.start();
-        b.start();
-        c.start();
-        d.start();
+
         /*
         List<Tribuna> tribune = List.of(a, b, c, d);
 
